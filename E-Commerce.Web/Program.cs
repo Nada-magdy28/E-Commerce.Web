1036f62669a3b73f49c9a896dc1cb8059af3e903
nada <nada@DESKTOP-T29DM50>
2025-04-16 18:12:23 +0200

    E

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,9 @@
 
+using DomainLayer.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Data;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -13,8 +18,23 @@ namespace E_Commerce.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            builder.Services.AddScoped<IDataSeeding, DataSeeding>();
 
             var app = builder.Build();
+
+
+
+          using var scope = app.Services.CreateScope();
+            var  ObjectOfDataSeeding = scope.ServiceProvider.GetRequiredService<IDataSeeding>();
+            ObjectOfDataSeeding.DataSeed();
+
+
+
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
